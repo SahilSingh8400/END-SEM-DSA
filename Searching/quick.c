f465fd6f7deaa11777265ac0b9a4e885b0e377db
SahilSingh8400 <sanju98099@gmail.com>
2026-01-06 01:28:27 +0530

    searching and sorting completed

--- Searching/quick.c
+++ Searching/quick.c
@@ -1,33 +1,53 @@
 #include <stdio.h>
 
-int partition(int a[], int low, int high) {
-    int pivot = a[high], i = low - 1, j, temp;
-    for(j = low; j < high; j++) {
-        if(a[j] < pivot) {
+int partition(int a[], int low, int high)
+{
+    int pivot = a[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+
+    while(i <= j)
+    {
+        while(i <= high && a[i] <= pivot)
             i++;
+
+        while(a[j] > pivot)
+            j--;
+
+        if(i < j)
+        {
             temp = a[i];
             a[i] = a[j];
             a[j] = temp;
         }
     }
-    temp = a[i+1];
-    a[i+1] = a[high];
-    a[high] = temp;
-    return i+1;
+
+    // Place pivot at correct position
+    temp = a[low];
+    a[low] = a[j];
+    a[j] = temp;
+
+    return j;
 }
 
-void quickSort(int a[], int low, int high) {
-    if(low < high) {
+void quickSort(int a[], int low, int high)
+{
+    if(low < high)
+    {
         int p = partition(a, low, high);
         quickSort(a, low, p - 1);
         quickSort(a, p + 1, high);
     }
 }
 
-int main() {
+int main()
+{
     int a[50], n, i;
 
+    printf("enter the size off array: ");
     scanf("%d", &n);
+    printf("enter the elements of array: \n");
     for(i = 0; i < n; i++)
         scanf("%d", &a[i]);
 
