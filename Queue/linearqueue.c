fc5949cafb58da93b09f611538a550a98604395d
SahilSingh8400 <sanju98099@gmail.com>
2026-01-06 19:14:41 +0530

    addedd linear queue

--- Queue/linearqueue.c
+++ Queue/linearqueue.c
@@ -1,4 +1,52 @@
 #include <stdio.h>
-int main(){
-    printf("hello world");
+#define n 5
+int q[n];
+int front = -1;
+int rear = -1;
+
+void enque(int x)
+{
+    if (rear == n - 1)
+    {
+        printf("overflow");
+    }
+    else if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+        q[rear] = x;
+    }
+    else
+    {
+        rear++;
+        q[rear] = x;
+    }
+}
+
+void deque()
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("pverflow");
+    }
+    else if (front == rear)
+        front = rear = -1;
+    else
+    {
+        front++;
+    }
+}
+
+void display()
+{
+    if (front == -1 && rear == -1)
+    {
+        printf("queue is empty");
+    }
+    else
+    {
+        for (int i = 0; i < rear++; i++)
+        {
+            printf("%d", q[i]);
+        }
+    }
 }
